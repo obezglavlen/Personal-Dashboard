65add38bce2725ee888cb44371786d25a3f80fd6
user <user@localhost>
2026-06-28 23:30:01 +0300

    chore: final cleanup + gitignore

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from ".prisma/client/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcryptjs";
 
